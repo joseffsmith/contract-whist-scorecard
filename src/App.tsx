46c1a82d9774f46d4a8517d69f28afaafb2eac0b
Joe Smith <joe@countfire.com>
2021-10-18 17:00:05 +0000

    more fighting iphone behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ const App: FunctionComponent<{ manager: Manager, scoreboard: Scoreboard }> = obs
   }
 
   return (
-    <div className="bg-gray-100 h-screen pb-8 flex flex-col overscroll-y-none">
+    <>
       <header className="flex justify-between p-1">
         <h1 className="text-lg font-semibold font-serif text-left my-2 inline-block">Contract whist</h1>
         {/* <button className="border px-2" onClick={share}>Share</button> */}
@@ -137,7 +137,7 @@ const App: FunctionComponent<{ manager: Manager, scoreboard: Scoreboard }> = obs
         <ScoreStage db={scoreboard} />
       }
 
-    </div>
+    </>
   )
 })
 
