d2a21f171ae0c14fa5043e819f34ee5a98a335ff
Joe Smith <joseffsmith@icloud.com>
2023-10-12 09:30:24 +0200

    update dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 // .prettierrc.js
 module.exports = {
-  printWidth: 120,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
+  // printWidth: 120,
+  // semi: false,
+  // singleQuote: true,
+  // trailingComma: 'all',
+  // bracketSpacing: true,
+  // jsxBracketSameLine: false,
 }
